a452a3e775b67886d6135c7e2acc11ffc3d93aec
Michelly Menezes <martha.michelly@gmail.com>
2017-05-15 12:52:02 -0300

    teste com post mais complexo

---
+++ _source/lab-1-parte-2/lab-1-parte-2.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/lab-1-parte-2/2017-05-15-lab-1-parte-2.Rmd
+
---
+++ _source/laboratório-1-parte-2/laboratório-1-parte-2.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/laboratório-1-parte-2/2017-05-15-laboratório-1-parte-2.Rmd
+
